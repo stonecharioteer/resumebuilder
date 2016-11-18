51dc3bb061f473f8ac6bdf8e0215725ddaca3ce6
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2016-11-18 22:02:53 +0530

    Added boilerplate code for the class definition.

--- resumebuilder/ResumeBuilder.py
+++ resumebuilder/ResumeBuilder.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """Resume Builder Class Definition"""
 
+class ResumeBuilder:
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def load_data(self, path_to_parse):
+        pass
+
